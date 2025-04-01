ae098c05623c7a3b50b23a4ff76f348f1b1374e6
simba1559295 <simba1@list.ru>
2025-04-01 14:19:42 +0300

    Add files via upload

---
+++ test bot.py
@@ -0,0 +1,11 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+x = input ("Введите ответ на первый вопрос ")
+if x == ("опухоль"):
+    print ("верно")
+    
+      
+    
+
+else:
+    print ("фигня")
